57a44b132d46b8391760683db87f7c29eb3f4f6e
Lilianada <lilibest30.lb@gmail.com>
2024-09-27 01:09:07 +0100

    started mobile header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,9 +20,9 @@ const navItems = [
   },
 ];
 
-export default function Header() {
+function DesktopHeader() {
   return (
-    <header className="w-[90%] flex justify-between items-center bg-blue rounded-xl px-6">
+    <div className="hidden w-full max-w-2xl md:flex justify-between items-center bg-blue rounded-xl px-2 py-2">
         <div className="flex text-xl gap-1">
             <Image src="/assets/Fitcreative-Logo.svg" alt="logo" width={100} height={50} />
         </div>
@@ -36,12 +36,32 @@ export default function Header() {
             {item.title}
           </Link>
         ))}
-      </nav>
-      <button>
         <Link href="/login" className="text-base font-medium bg-yellow text-gray-700 px-4 py-2 rounded-lg">
             Join community
         </Link>
-      </button>
+      </nav>
+    </div>
+  );
+}
+
+function MobileHeader() {
+    return (
+        <div className="flex w-full max-w-2xl md:hidden justify-between items-center bg-blue rounded-lg px-2 py-3">
+        <nav className="flex lg:hidden gap-6 items-center justify-center text-gray-100">
+        <div className="flex text-xl gap-1">
+            <Image src="/assets/Logo-icon.svg" alt="logo" width={60} height={50} />
+        </div>
+        {/* <button className="text-base font-medium hover:text-gray-900">Menu</button> */}
+        </nav>
+        </div>
+    );
+    }
+
+export default function Header() {
+  return (
+    <header className="w-[90%] m-auto grid place-items-center">
+        <DesktopHeader />
+        <MobileHeader />
     </header>
   );
 }
