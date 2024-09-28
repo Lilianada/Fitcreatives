261b4a9d63a74c0006302e2211eb68a5d2d7065f
Lilianada <lilibest30.lb@gmail.com>
2024-09-28 23:58:03 +0100

    first hero design completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,24 +1,28 @@
+import Image from "next/image";
 import React from "react";
 
 export default function Hero() {
   return (
     <section className="grid place-items-center w-full mt-16 ">
-      <div className="bg-yellow px-3 py-1 w-fit rounded-lg">
-        <p className="text-gray-700">Fitcreatives - Online fitness community</p>
+      <div className="bg-blue mt-4 w-full p-6 rounded-xl relative grid place-items-center h-[500px] content-start">
+      <div className=" px-3 py-1 w-fit rounded-lg">
+        <p className="text-yellow text-xs">Fitcreatives - Online fitness community</p>
       </div>
-      <div className="bg-blue mt-4 w-full p-6 rounded-xl">
         <div className="flex flex-col justify-center items-center text-center w-full mb-12">
-          <div className="w-2/3 mt-4">
+          <div className="w-full md:w-2/3 mt-4">
             <h1 className="text-6xl font-bold text-gray">
               Stay Fit
               <br /> Stay Creative
             </h1>
-            <p className="text-lg mt-2 text-center">
+            <p className="w-full text-lg mt-2 text-center">
               Join a supportive community of creatives who prioritize fitness.
               Get expert guidance, community support, and tailored workouts.
             </p>
           </div>
         </div>
+      <div className="bg-yellow h-72 md:h-[400px] w-[90%] md:w-[85%] rounded-xl absolute top-2/3 md:top-[60%]">
+        <Image src="/assets/IMG_6380.jpg" alt="hero image" layout="fill" objectFit="cover" className="rounded-xl" />
+      </div>
       </div>
     </section>
   );
