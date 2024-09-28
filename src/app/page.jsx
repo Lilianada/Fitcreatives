a6a03d340409dbace4e35931a296d5738854d237
Lilianada <lilibest30.lb@gmail.com>
2024-09-28 22:15:58 +0100

    hero section contents update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,13 @@
 'use client';
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 
 
 export default function Home() {
   return (
     <div className="grid items-center justify-items-center p-4 sm:px-8 sm:py-4">
-      <Header/>
-      
+      <Header />
+      <Hero />
     </div>
   );
 }
