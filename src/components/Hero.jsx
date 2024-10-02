c66075efa0d529c1f2da462200bf4d5543b00256
Lilianada <lilibest30.lb@gmail.com>
2024-10-02 15:40:52 +0100

    hero fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,20 +15,20 @@ export default function Hero() {
           src="/assets/Untitled-design.svg"
           className="absolute inset-0 -z-10 h-full w-full object-cover object-right rounded-xl"
         />
-        <div className="mx-auto max-w-2xl py-20 px-8">
-          <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+        <div className="mx-auto sm:mx-0 max-w-2xl py-20 px-8">
+          <div className="hidden sm:mb-8 sm:flex justify-center sm:justify-start">
             <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-light ring-1 ring-black/10 hover:ring-white/20">
             Stay Fit, Stay Creative
             </div>
           </div>
-          <div className="text-center">
+          <div className="text-center sm:text-left">
             <h1 className="text-4xl font-bold tracking-tight text-yellow bg-clip-text sm:text-6xl">
             A community of like minded individuals that support your fitness journey.
             </h1>
             <p className="mt-6 text-lg leading-8 text-light">
             Discover personalized workouts, expert advice, and a supportive community at FitCreatives.
             </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6">
+            <div className="mt-10 flex items-center justify-center sm:justify-start gap-x-6">
               <a
                 href="#"
                 className="rounded-md bg-lightblue px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-lightblue focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-lightblue"
