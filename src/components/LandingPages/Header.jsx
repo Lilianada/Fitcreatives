9b22d16337a63116141c6cd8bb44bb3156dfbfcc
Lilianada <lilibest30.lb@gmail.com>
2024-10-09 20:36:32 +0100

    removed home route from header

--- src/components/LandingPages/Header.jsx
+++ src/components/LandingPages/Header.jsx
@@ -2,26 +2,15 @@
 import Image from "next/image";
 import Link from "next/link";
 import { ModeToggle } from "./ModeToggle";
-import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { HomeIcon, PersonIcon } from "@radix-ui/react-icons";
 import { BiParty } from "react-icons/bi";
 
 import {
   NavigationMenu,
-  NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
-  navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
-import { MapPinIcon } from "@heroicons/react/24/outline";
+import { MapPinIcon, ShoppingCartIcon, UsersIcon } from "@heroicons/react/24/outline";
 
 function MenuButtons({ icon: Icon, text, href }) {
   return (
@@ -32,7 +21,6 @@ function MenuButtons({ icon: Icon, text, href }) {
             <NavigationMenuItem>
               <Link href={href} legacyBehavior passHref className="p-0">
                 <NavigationMenuLink className="relative flex items-center justify-center w-14 h-10 p-2 rounded-md hover:bg-zinc-100 dark:hover:bg-zinc-800 border border-zinc-300 dark:border-zinc-700 transition-colors duration-300">
-                  {/* Icon with transition fixes */}
                   <Icon
                     className={`h-[1.2rem] w-[1.2rem] transition-transform duration-500 text-zinc-900 dark:text-orange-400`}
                   />
@@ -62,10 +50,10 @@ export default function Header() {
           <h2>Fitcreatives</h2>
         </div>
         <div className="flex gap-3">
-          <MenuButtons icon={HomeIcon} text="Home" href="/" />
-          <MenuButtons icon={PersonIcon} text="Trainers" href="/trainers" />
+          <MenuButtons icon={UsersIcon} text="Trainers" href="/trainers" />
           <MenuButtons icon={MapPinIcon} text="Find Gym" href="/gym" />
           <MenuButtons icon={BiParty} text="Events" href="/events" />
+          <MenuButtons icon={ShoppingCartIcon} text="Shop" href="/shop" />
           <ModeToggle />
         </div>
       </div>
--- src/components/LandingPages/ModeToggle.jsx
+++ src/components/LandingPages/ModeToggle.jsx
@@ -1,13 +1,11 @@
 import { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
 import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
-// import { MoonIcon, SunIcon } from "@heroicons/react/24/solid"; // Assuming you're using Heroicons
 
 export function ModeToggle() {
   const { theme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
 
-  // Ensures the theme is applied after mounting
   useEffect(() => setMounted(true), []);
 
   if (!mounted) return null; // Prevents hydration mismatch
@@ -22,7 +20,6 @@ export function ModeToggle() {
       className="relative flex items-center justify-center w-14 h-10 p-2 rounded-md hover:bg-zinc-100 dark:hover:bg-zinc-800 border border-zinc-300 dark:border-zinc-700 transition-colors duration-300"
       aria-label="Toggle theme"
     >
-      {/* Render both icons but control visibility with scale and rotate */}
       <SunIcon
         className={`h-[1.5rem] w-[1.5rem] absolute transition-transform duration-500 ${
           theme === "dark"
