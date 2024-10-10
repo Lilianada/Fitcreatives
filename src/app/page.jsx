3a4a1f3e2ab9be75d4334362fc249a3cb2a894e5
Lilianada <lilibest30.lb@gmail.com>
2024-10-10 11:51:00 +0100

    changed frequently asked questions

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,7 @@ import CallToAction from "@/components/LandingPages/CallToAction";
 import FrequentlyAskedQtn from "@/components/LandingPages/Faq";
 import Hero from "@/components/LandingPages/Hero";
 
-export default function Index() {
+export default function Home() {
   return (
     <div className="w-full min-h-[calc(100vh_-_12rem)] grid gap-8">
       <Hero />
