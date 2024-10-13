efa2fd85a4e49180529364c71aea2f323bd96298
Lilianada <lilibest30.lb@gmail.com>
2024-10-13 22:00:57 +0100

    added section to explain the process of joining

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,14 @@
 import { MissionSection } from "@/components/home/Mission";
 import FrequentlyAskedQtn from "@/components/home/Faq";
 import Hero from "@/components/home/Hero";
+import Process from "@/components/home/Process";
 
 export default function Home() {
   return (
     <div className="w-full grid gap-8">
       <Hero />
       <MissionSection/>
+      <Process />
       <FrequentlyAskedQtn />
     </div>
   );
---
+++ src/components/home/Process.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Process() {
+  return (
+    <section className='border p-4'>
+      
+    </section>
+  )
+}
